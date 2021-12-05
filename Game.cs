340ecddd5fc210f1067fc98ffabe50bc59c00ade
xaban3r <nstr3@mail.ru>
2021-12-06 01:35:33 +0500

    andMore

--- Game.cs
+++ Game.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Logic.TetrisFigure;
 
-namespace t3tr1s
+namespace Logic
 {
     class Game
     {
+        public int Score { get; private set; } = 0;
+        public int Lvl { get; private set; } = 0;
+        public bool IsEndGame { get; private set; } = false;
+        
+       
+
+        public static readonly int Rows = 20;
+        public static readonly int Columns = 10;
+
+        private readonly Random random = new Random();
+
+
     }
 }
+
